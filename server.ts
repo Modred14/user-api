c61e1e70130434b6b629fe46ba0847039a0270ca
Modred14 <favourdomirin@gmail.com>
2024-08-13 00:53:17 -0700

    tenth commit

--- server.ts
+++ server.ts
@@ -117,11 +117,11 @@ app.post("/users", async (req: Request, res: Response) => {
     firstName,
     lastName,
     email,
-    password,
     profileImg:
       profileImg ||
       "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     links: formattedLinks,
+    ...(password && { password }),
   };
 
   try {
