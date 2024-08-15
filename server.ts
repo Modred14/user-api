f35474e93ae3eae856c35dc95d410070ef06a1cf
Modred14 <favourdomirin@gmail.com>
2024-08-15 05:30:04 -0700

    change the profileImg

--- server.ts
+++ server.ts
@@ -2,7 +2,6 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { v4 as uuidv4 } from "uuid";
-import whois from "whois-json";
 import admin from "firebase-admin";
 // Initialize Firebase
 const serviceAccount = require("./scissors-altschool-favour-firebase-adminsdk-3jha8-3f022b5646.json");
@@ -119,7 +118,7 @@ app.post("/users", async (req: Request, res: Response) => {
     email,
     profileImg:
       profileImg ||
-      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      "https://cdn-icons-png.flaticon.com/512/847/847969.png",
     links: formattedLinks,
     ...(password && { password }),
   };
